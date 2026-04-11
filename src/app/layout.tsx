3550d692bef24a28c26f11cc235a3d9917ef9af2
Texas Holdem Dev <dev@texasholdem.local>
2026-04-11 13:28:45 +1000

    chore: remove unused eslint-disable comment in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,6 @@ export default async function RootLayout({
   return (
     <html lang={locale} className="min-h-dvh">
       <head>
-        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2430817783068325"
