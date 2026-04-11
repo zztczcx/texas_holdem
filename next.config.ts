8b0058d247f16d968be8ee3621ef1829dd8bd949
Texas Holdem Dev <dev@texasholdem.local>
2026-04-11 13:50:52 +1000

    fix: allow adtrafficquality.google in CSP img-src

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self' 'unsafe-eval' 'unsafe-inline' https://js.pusher.com https://va.vercel-scripts.com https://pagead2.googlesyndication.com https://partner.googleadservices.com https://tpc.googlesyndication.com https://www.googletagservices.com https://adservice.google.com https://*.adtrafficquality.google;
   style-src 'self' 'unsafe-inline';
-  img-src 'self' data: blob: https://*.googlesyndication.com https://*.doubleclick.net https://*.google.com;
+  img-src 'self' data: blob: https://*.googlesyndication.com https://*.doubleclick.net https://*.google.com https://*.adtrafficquality.google;
   font-src 'self';
   connect-src 'self' wss://*.pusher.com https://*.pusher.com https://*.upstash.io https://va.vercel-scripts.com https://pagead2.googlesyndication.com https://*.doubleclick.net https://adservice.google.com https://*.adtrafficquality.google https://*.googlesyndication.com;
   frame-src https://googleads.g.doubleclick.net https://tpc.googlesyndication.com https://www.google.com https://*.adtrafficquality.google;
